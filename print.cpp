f1e8670f621ef488afc1d5ebca4288cff4429d10
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-08 23:03:48 -0500

    Add print.cpp with 1000 print calls (no loops)

---
+++ print.cpp
@@ -0,0 +1,13 @@
+#include <stdio.
+
+using namespace std;
+
+int main() {
+    printf("print 1\n");
+    printf("print 2\n");
+    printf("print 3\n");
+    printf("print 4\n");
+    // ... and so on
+    printf("print 1000\n");
+    return 0;
+}
