a049ccb3a0507fdd0a1752dec035c068005d4404
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-08 23:05:22 -0500

    Actually write out 1000 printf statements, no loops

--- print.cpp
+++ print.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 
 int main() {
-    printf("line 1");
-    printf("line 2");
-    printf("line 3");
-    printf("line 4");
-    printf("line 5");
-    ...
-    printf("line 1000");
+    printf("print 1\n");
+    printf("print 2\n");
+    printf("print 3\n");
+    printf("print 4\n");
+    printf("print 5\n");
+    printf("print 6\n");
+    printf("print 7\n");
+    printf("print 8\n");
+    printf("print 9\n");
+    printf("print 10\n");
+    printf("print 11\n");
+    printf("print 12\n");
+    printf("print 13\n");
+    //... 20 ...
+    printf("print 1000\n");
     return 0;
 }
